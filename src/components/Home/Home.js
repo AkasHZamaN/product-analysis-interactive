2f45e4edf9a401420623bb316258ff86102dcbbb
AkasHZamaN <akashzaman4@gmail.com>
2022-04-04 07:36:32 +0600

    reviews area ended

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,4 @@
+import { ShoppingCartIcon } from "@heroicons/react/solid";
 import React from "react";
 import laptop from "./img/laptop.png"
 const Home = () => {
@@ -13,7 +14,7 @@ const Home = () => {
             with fast, dedicated expert help whenever you need it. Get one year
             of Premium Support included when you purchase an XPS laptop.
           </p>
-          <button className="border-0 py-2 px-6 mt-3 bg-teal-500 hover:bg-teal-600 text-white font-semibold rounded-xl">Buy Now</button>
+          <button className="flex border-0 py-2 px-6 mt-3 bg-teal-500 hover:bg-teal-600 text-white font-semibold rounded-xl">BUY NOW <ShoppingCartIcon className="w-6 h-6 ml-2 "></ShoppingCartIcon></button>
         </article>
         <article>
             <img src={laptop} alt="" />
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import useReviews from '../../hooks/useReviews';
+import React from "react";
+import useReviews from "../../hooks/useReviews";
+import ReviewUser from "../ReviewUser/ReviewUser";
 
 const Reviews = () => {
-    const [reviews, setReviews] = useReviews();
-    return (
-        <div>
-            <h2>Welcome to reviews section: {reviews.length}</h2>
-        </div>
-    );
+  const [reviews] = useReviews();
+  return (
+    <section >
+      <article className="text-right mr-10">
+        <h2 className="text-2xl font-semibold font-mono text-slate-500 my-5">Top Ten Reviewer's in Sylhet</h2>
+      </article>
+      <article className="grid grid-cols-3 gap-4 justify-items-center mx-10 mb-10">
+          {
+              reviews.map(item => <ReviewUser key={item.id} item={item}></ReviewUser>)
+          }
+      </article>
+    </section>
+  );
 };
 
 export default Reviews;
