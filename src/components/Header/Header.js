f48840e87820ad5a95182f6edbe6e46ac74500f9
AkasHZamaN <akashzaman4@gmail.com>
2022-04-04 11:45:37 +0600

    modified somefiles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,12 @@ const Header = () => {
             <div className='text-center text-4xl font-mono font-semibold bg-slate-500 text-white py-8'>
             <h1>Online Products Reviewer's</h1>
         </div>
-        <nav className='text-center bg-teal-500 text-white py-3 font-semibold'>
-            <Link className='mr-5' to="/">HOME</Link>
-            <Link className='mr-5' to="/reviews">REVIEWS</Link>
-            <Link className='mr-5' to="/dashboard">DASHBOARD</Link>
-            <Link className='mr-5' to="/blogs">BLOGS</Link>
-            <Link to="/about">ABOUT</Link>
+        <nav className='text-center bg-teal-500  text-white py-3 font-semibold'>
+            <Link className='mr-16 hover:text-teal-800' to="/">HOME</Link>
+            <Link className='mr-16 hover:text-teal-800' to="/reviews">REVIEWS</Link>
+            <Link className='mr-16 hover:text-teal-800' to="/dashboard">DASHBOARD</Link>
+            <Link className='mr-16 hover:text-teal-800' to="/blogs">BLOGS</Link>
+            <Link className='mr-16 hover:text-teal-800' to="/about">ABOUT</Link>
         </nav>
         </section>
         
