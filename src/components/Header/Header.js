9c9a8ccaf0f7d088ee1e1d6fbef7bd3f5a7f3e02
AkasHZamaN <akashzaman4@gmail.com>
2022-04-04 19:08:13 +0600

    modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from './logo/Online_logo_2.png';
 
 const Header = () => {
   return (
     <section>
-      <div className="text-center text-4xl font-mono font-semibold bg-slate-500 text-white py-8">
-        <h1>Online Products Reviewer's</h1>
-      </div>
+      <article className=" flex justify-between items-center text-center text-4xl font-mono font-semibold bg-slate-500 text-white py-8 px-16">
+        <img className="w-40 h-16 mr-2 bg-orange-50 rounded-2xl" src={logo} alt="" />
+        <h1>Products Reviewer's</h1>
+      </article>
       <nav className="text-center bg-teal-500  text-white py-3 font-semibold">
         <Link className="mr-16 hover:text-teal-800" to="/">
           HOME
