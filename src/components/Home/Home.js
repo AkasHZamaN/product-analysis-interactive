bc6e26637e5173f417d16477a54ee4588b59c70a
AkasHZamaN <akashzaman4@gmail.com>
2022-04-04 11:29:18 +0600

    blogs ended and home modified

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,9 @@
-import { ShoppingCartIcon } from "@heroicons/react/solid";
+import { ArrowRightIcon, ShoppingCartIcon } from "@heroicons/react/solid";
 import React from "react";
+import { Link } from "react-router-dom";
 import useReviews from "../../hooks/useReviews";
 import HomeReview from "../HomeReview/HomeReview";
-import laptop from "./img/laptop.png"
+import laptop from "./img/laptop.png";
 const Home = () => {
   const [reviews] = useReviews();
   return (
@@ -10,27 +11,41 @@ const Home = () => {
       <article className="flex justify-between items-center mx-16 mt-24">
         <article>
           <h1 className="text-3xl text-teal-500 font-mono">
-            Dell XPS 17 HANDS-ON Preview! <span className="text-slate-500">Amazing Future Generation</span>
+            Dell XPS 17 HANDS-ON Preview!{" "}
+            <span className="text-slate-500">Amazing Future Generation</span>
           </h1>
           <p className=" text-gray-500 font-semibold mt-10">
             XPS with Premium Support helps you stay connected and productive
             with fast, dedicated expert help whenever you need it. Get one year
             of Premium Support included when you purchase an XPS laptop.
           </p>
-          <button className="flex border-0 py-2 px-6 mt-3 bg-teal-500 hover:bg-teal-600 text-white font-semibold rounded-xl">BUY NOW <ShoppingCartIcon className="w-6 h-6 ml-2 "></ShoppingCartIcon></button>
+          <button className="flex border-0 py-2 px-6 mt-3 bg-teal-500 hover:bg-teal-600 text-white font-semibold rounded-xl">
+            BUY NOW{" "}
+            <ShoppingCartIcon className="w-6 h-6 ml-2 "></ShoppingCartIcon>
+          </button>
         </article>
         <article>
-            <img src={laptop} alt="" />
+          <img src={laptop} alt="" />
         </article>
       </article>
       <article>
-          <h1 className="text-right mr-16 text-2xl font-mono mt-28 text-slate-500 font-semibold mb-10">Customer Reviews</h1>
-          <article className="grid grid-cols-3 gap-4 mx-16 mb-10">
-            {
-              reviews.map(homeReview =><HomeReview key={homeReview.id} homeReview={homeReview}></HomeReview>)
-            }
-          </article>
+        <h1 className="text-right mr-16 text-2xl font-mono mt-28 text-slate-500 font-semibold mb-10">
+          Customer Reviews
+        </h1>
+        <article className="grid grid-cols-3 gap-4 mx-16 mb-10">
+          {reviews.slice(0, 3).map((homeReview) => (
+            <HomeReview
+              key={homeReview.id}
+              homeReview={homeReview}
+            ></HomeReview>
+          ))}
+        </article>
       </article>
+      <Link to="/reviews">
+        <button className="bg-teal-500 py-2 px-5 text-white rounded-xl font-semibold hover:bg-teal-600 flex mx-auto mb-10">
+          SEE MORE <ArrowRightIcon className="w-6 h-6 ml-2"></ArrowRightIcon>
+        </button>
+      </Link>
     </section>
   );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -6,7 +6,7 @@ const Reviews = () => {
   const [reviews] = useReviews();
   return (
     <section >
-      <article className="text-right mr-10">
+      <article className="text-right mr-12">
         <h2 className="text-2xl font-semibold font-mono text-slate-500 my-5">Top Ten Reviewer's in Sylhet</h2>
       </article>
       <article className="grid grid-cols-3 gap-4 justify-items-center mx-10 mb-10">
